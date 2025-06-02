e248bdfe7cf357e51e18d67dd05a387f01649cfa
Sai Prasanth Madala <saiprasanth1425@gmail.com>
2025-06-02 09:48:44 +0530

    Update python_whatsapp_msgs.py

--- python_whatsapp_msgs.py
+++ python_whatsapp_msgs.py
@@ -1,2 +1,2 @@
 import pywhatkit as pwk
-pwk.("phonenumber(with country code)","message",time in hr(24 hr format),time in min,wait time(in sec)-default(15),tab close(true or false),tab close time (in sec)-default(3 sec))
+pwk.("phonenumber(with country code)","message",time in hr(24 hr format),time in min,wait time(in sec)-default(15 sec),tab close(True or False),tab close time (in sec)-default(3 sec))
