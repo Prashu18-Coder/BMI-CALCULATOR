cade2588b3616fc0a02b962d2247ecf46d272b49
Sai Prasanth Madala <saiprasanth1425@gmail.com>
2025-06-02 09:47:10 +0530

    Add files via upload

---
+++ python_whatsapp_msgs.py
@@ -0,0 +1,2 @@
+import pywhatkit as pwk
+pwk.("phonenumber(with country code)","message",time in hr(24 hr format),time in min,wait time(in sec)-default(15),tab close(true or false),tab close time (in sec)-default(3 sec))
