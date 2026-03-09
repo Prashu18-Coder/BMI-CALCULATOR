81285db724c8d6f11cc8879fba21db6fecf5c06c
Sai Prasanth Madala <saiprasanth1425@gmail.com>
2026-03-09 07:21:38 +0530

    Delete python_whatsapp_msgs.py

--- python_whatsapp_msgs.py
+++
@@ -1,2 +0,0 @@
-import pywhatkit as pwk
-pwk.("phonenumber(with country code)","message",time in hr(24 hr format),time in min,wait time(in sec)-default(15 sec),tab close(True or False),tab close time (in sec)-default(3 sec))
